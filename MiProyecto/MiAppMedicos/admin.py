1b3e7eef54963572f82d825b489bffaa50ca92e3
unknown <walterarielpelourson@yahoo.com.ar>
2024-12-13 18:47:31 -0300

    Cuarto  Commit

--- MiProyecto/MiAppMedicos/admin.py
+++ MiProyecto/MiAppMedicos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin # type: ignore
+from MiAppMedicos.models import MedicoModels
+from MiAppMedicos.models import PacienteModels
 
 # Register your models here.
+
+admin.site.register(MedicoModels)
+admin.site.register(PacienteModels)
+
--- MiProyecto/MiAppMedicos/models.py
+++ MiProyecto/MiAppMedicos/models.py
@@ -7,7 +7,10 @@ class MedicoModels(models.Model):
     apellido = models.CharField(max_length=20)
     matricula = models.CharField(max_length=8)
     profesion = models.CharField(max_length=30)
+    def __str__(self):
+        return  f"{self.id} | Nombre: {self.nombre} | Apellido: {self.apellido} | Profesion: {self.profesion}"
     
+    ()
 #Tabla Clientes
 class PacienteModels(models.Model):
     
@@ -16,3 +19,5 @@ class PacienteModels(models.Model):
     obrasocial = models.CharField(max_length=8)
     edad = models.IntegerField()
     fechanacimiento = models.DateField()
+    def __str__(self):
+        return  f"{self.id} | Nombre: {self.nombre} | Apellido: {self.apellido} | Profesion: {self.obrasocial}"
